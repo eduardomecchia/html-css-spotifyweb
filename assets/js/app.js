a71b1bebbb1619c5db75372c7af2082109a445b9
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-04-21 13:46:15 +0200

    Update playlist names and other details using JavaScript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
-document.getElementById('heavymetal').innerHTML = 'Various Artists'
-document.getElementById('strangerthings').innerHTML = 'Kyle Dixon & Michael Stain'
-document.getElementById('aquietplace').innerHTML = 'Marco Beltrami'
-document.getElementById('split').innerHTML = 'West Thordson'
-document.getElementById('acureforwellness').innerHTML = 'Benjamin Wallfisch'
-document.getElementById('sinister').innerHTML = 'Cristopher Young'
+// Assign names to playlists in sidebar
+
+document.getElementById('playlist_1').innerHTML = 'Heavy Metal';
+document.getElementById('playlist_2').innerHTML = 'Stranger Things';
+document.getElementById('playlist_3').innerHTML = 'A Quiet Place';
+document.getElementById('playlist_4').innerHTML = 'Split';
+document.getElementById('playlist_5').innerHTML = 'A Cure For Wellness';
+document.getElementById('playlist_6').innerHTML = 'Sinister';
+document.getElementById('playlist_7').innerHTML = 'Release Radar';
+
+// Assign playlist authors' names
+
+document.getElementById('heavymetal').innerHTML = 'Various Artists';
+document.getElementById('strangerthings').innerHTML = 'Kyle Dixon & Michael Stain';
+document.getElementById('aquietplace').innerHTML = 'Marco Beltrami';
+document.getElementById('split').innerHTML = 'West Thordson';
+document.getElementById('acureforwellness').innerHTML = 'Benjamin Wallfisch';
+document.getElementById('sinister').innerHTML = 'Cristopher Young';
+
+// Assign name to profile
+
+document.getElementById('profile_name').innerHTML = 'Eduardo Mecchia';
+
+// Assign name to playlist row
+
+document.getElementById('created_for').innerHTML = 'Creato per Eduardo Mecchia';
